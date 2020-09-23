e7452b7ba8123eac75cc0ab5165bc43206d1abc9
Jason Rash <jasonmichaelrash@gmail.com>
2020-09-23 12:10:12 -0500

    changed setup inputs to type range and working on labels

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,12 +1,19 @@
 <?php
-  $url = getenv('JAWSDB_URL');
-  $dbparts = parse_url($url);
+  if (getenv('JAWSDB_URL')){
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
   
-  $hostname = $dbparts['host'];
-  $username = $dbparts['user'];
-  $password = $dbparts['pass'];
-  $database = ltrim($dbparts['path'],'/');
-
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+  }
+  else { // For Development
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "loginsystem";
+  }
   $conn = mysqli_connect($hostname, $username, $password, $database);
 
   if (!$conn) {
--- setups.php
+++ setups.php
@@ -75,24 +75,29 @@
                     <strong class="justify-content-center">Aerodynamics</strong>
                   </div>
                   <div>
-                    <input class="form-control form-control-sm" type="number" min="1" max="11" title="Front Wing" name="frontWing" placeholder="Front Wing (1-11)">
-                    <input class="form-control form-control-sm" type="number" min="1" max="11" title="Rear Wing" name="rearWing" placeholder="Rear Wing (1-11)">
+                    <div class="text-center">Front Wing (1-11)</div>
+                    <input class="form-control form-control-sm" type="range" step="1" min="1" max="11" title="Front Wing" name="frontWing">
+                    <div class="text-center">Rear Wing (1-11)</div>
+                    <input class="form-control form-control-sm" type="range" step="1" min="1" max="11" title="Rear Wing" name="rearWing">
                   </div>
                   <div class="text-center">
                   <strong class="justify-content-center">Transmission</strong>
                 </div>
                 <div>
-                  <input class="form-control form-control-sm" type="number" min="50" max="100" title="On Throttle Differential" name="onThrottleDiff" placeholder="On Throttle Diff (50-100)">
-                  <input class="form-control form-control-sm" type="number" min="50" max="100" title="Off Throttle Differential" name="offThrottleDiff" placeholder="Off Throttle Diff (50-100)">
+                  <div class="text-center">On Throttle Differential (50-100)</div>
+                  <input class="form-control form-control-sm" type="range" step="5" min="50" max="100" title="On Throttle Differential" name="onThrottleDiff">
+                  <div class="text-center">Off Throttle Differential (50-100)</div>
+                  <input class="form-control form-control-sm" type="range" step="5" min="50" max="100" title="Off Throttle Differential" name="offThrottleDiff">
                 </div>
                 <div class="text-center">
                   <strong class="justify-content-center">Suspension Geometry</strong>
                 </div>
                 <div>
-                  <input class="form-control form-control-sm" type="number" min="-3.50" max="-2.50" step="0.01" title="Front Camber" name="frontCamber" placeholder="Front Camber (-3.50 to -2.50)">
-                  <input class="form-control form-control-sm" type="number" min="-2.00" max="-1.00" step="0.01" title="Rear Camber" name="rearCamber" placeholder="Rear Camber (-2.00 to -1.00)">
-                  <input class="form-control form-control-sm" type="number" min="0.05" max="0.15" step="0.01" title="Front Toe" name="frontToe" placeholder="Front Toe (0.05 to 0.15)">
-                  <input class="form-control form-control-sm" type="number" min="0.20" max="0.50" step="0.01" title="Rear Toe" name="rearToe" placeholder="Rear Toe (0.20 to 0.50)">
+                  <div class="text-center">Front Camber (-3.50 to -2.50)</div>
+                  <input class="form-control form-control-sm" type="range" min="-3.50" max="-2.50" step="0.10" title="Front Camber" name="frontCamber">
+                  <input class="form-control form-control-sm" type="range" min="-2.00" max="-1.00" step="0.10" title="Rear Camber" name="rearCamber" placeholder="Rear Camber (-2.00 to -1.00)">
+                  <input class="form-control form-control-sm" type="range" min="0.05" max="0.15" step="0.01" title="Front Toe" name="frontToe" placeholder="Front Toe (0.05 to 0.15)">
+                  <input class="form-control form-control-sm" type="range" min="0.20" max="0.50" step="0.03" title="Rear Toe" name="rearToe" placeholder="Rear Toe (0.20 to 0.50)">
                 </div>
                 </div>
               <div class="col-lg">
@@ -101,12 +106,12 @@
                   <strong class="justify-content-center">Suspension</strong>
                 </div>
                 <div>
-                  <input class="form-control form-control-sm" type="number" min="1" max="11" title="Front Suspension" name="frontSuspension" placeholder="Front Suspension (1-11)">
-                  <input class="form-control form-control-sm" type="number" min="1" max="11" title="Rear Suspension" name="rearSuspension" placeholder="Rear Suspension (1-11)">
-                  <input class="form-control form-control-sm" type="number" min="1" max="11" title="Front Anti-Roll Bar" name="frontAntiRoll" placeholder="Front Anti-Roll Bar (1-11)">
-                  <input class="form-control form-control-sm" type="number" min="1" max="11" title="Rear Anti-Roll Bar" name="rearAntiRoll" placeholder="Rear Anti-Roll Bar (1-11)">
-                  <input class="form-control form-control-sm" type="number" min="1" max="11" title="Front Ride Height" name="frontRideHeight" placeholder="Front Ride Height (1-11)">
-                  <input class="form-control form-control-sm" type="number" min="1" max="11" title="Rear Ride Height" name="rearRideHeight" placeholder="Rear Ride Height (1-11)">
+                  <input class="form-control form-control-sm" type="range" step="1" min="1" max="11" title="Front Suspension" name="frontSuspension" placeholder="Front Suspension (1-11)">
+                  <input class="form-control form-control-sm" type="range" step="1" min="1" max="11" title="Rear Suspension" name="rearSuspension" placeholder="Rear Suspension (1-11)">
+                  <input class="form-control form-control-sm" type="range" step="1" min="1" max="11" title="Front Anti-Roll Bar" name="frontAntiRoll" placeholder="Front Anti-Roll Bar (1-11)">
+                  <input class="form-control form-control-sm" type="range" step="1" min="1" max="11" title="Rear Anti-Roll Bar" name="rearAntiRoll" placeholder="Rear Anti-Roll Bar (1-11)">
+                  <input class="form-control form-control-sm" type="range" step="1" min="1" max="11" title="Front Ride Height" name="frontRideHeight" placeholder="Front Ride Height (1-11)">
+                  <input class="form-control form-control-sm" type="range" step="1" min="1" max="11" title="Rear Ride Height" name="rearRideHeight" placeholder="Rear Ride Height (1-11)">
                 </div>
               </div>
               <div class="col-lg">
@@ -114,17 +119,17 @@
                   <strong class="justify-content-center">Brakes</strong>
                 </div>
                 <div class="">
-                  <input class="form-control form-control-sm" type="number" min="50" max="100" title="Brake Pressure" name="brakePressure" placeholder="Brake Pressure (50-100)">
-                  <input class="form-control form-control-sm" type="number" min="50" max="70" title="Front Brake Bias" name="frontBrakeBias" placeholder="Front Brake Bias (70-50)">
+                  <input class="form-control form-control-sm" type="range" min="50" max="100" title="Brake Pressure" name="brakePressure" placeholder="Brake Pressure (50-100)">
+                  <input class="form-control form-control-sm" type="range" min="50" max="70" title="Front Brake Bias" name="frontBrakeBias" placeholder="Front Brake Bias (70-50)">
                 </div>
                 <div class="text-center">
                   <strong class="justify-content-center">Tyres</strong>
                 </div>
                 <div class="">
-                  <input class="form-control form-control-sm" type="number" min="21.0" max="25.0" step="0.1" name="frontRightPressure" placeholder="Front Right Tyre Pressure (21.0 - 25.0)">
-                  <input class="form-control form-control-sm" type="number" min="21.0" max="25.0" step="0.1" name="frontLeftPressure" placeholder="Front Left Tyre Pressure (21.0 - 25.0)">
-                  <input class="form-control form-control-sm" type="number" min="19.5" max="23.5" step="0.1" name="rearRightPressure" placeholder="Rear Right Tyre Pressure (19.5 - 23.5)">
-                  <input class="form-control form-control-sm" type="number" min="19.5" max="23.5" step="0.1" name="rearLeftPressure" placeholder="Rear Left Tyre Pressure (19.5 - 23.5)">
+                  <input class="form-control form-control-sm" type="range" min="21.0" max="25.0" step="0.4" name="frontRightPressure" placeholder="Front Right Tyre Pressure (21.0 - 25.0)">
+                  <input class="form-control form-control-sm" type="range" min="21.0" max="25.0" step="0.4" name="frontLeftPressure" placeholder="Front Left Tyre Pressure (21.0 - 25.0)">
+                  <input class="form-control form-control-sm" type="range" min="19.5" max="23.5" step="0.4" name="rearRightPressure" placeholder="Rear Right Tyre Pressure (19.5 - 23.5)">
+                  <input class="form-control form-control-sm" type="range" min="19.5" max="23.5" step="0.4" name="rearLeftPressure" placeholder="Rear Left Tyre Pressure (19.5 - 23.5)">
                 </div>
               </div>
               </div>
